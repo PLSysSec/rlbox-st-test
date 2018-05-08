4b8a62f639d167c34d1cfb77a1c9d383bebe93f1
Deian Stefan <deian@cs.ucsd.edu>
2018-05-07 23:02:55 -0700

    no cache

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const st = require('st')
 const http = require('http')
  
 http.createServer(
-  st(__dirname + '/static')
+  st( { path: __dirname + '/static'
+      , cache : { content: false }
+      })
 ).listen(1337)
