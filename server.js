7088f55d51fc201e099650cca25a8de087b6f363
shravanrn@gmail.com <shravanrn@gmail.com>
2019-02-26 01:43:19 -0800

    Reduce to one server and make images use localhost url

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const st = require('st')
-const http = require('http')
+'use strict';
+const express = require('express')
+const app = express()
+
+app.use(express.static('static'))
+
+function mkImages(nr) {
+  let str = "";
+  for(let i = 0; i < nr; i++) {
+    str += `<img with="10px" height="10px" src="http://host${i}.localhost:1337/img.jpeg"/>\n`;
+  }
+  return str;
+}
  
-mount = st( { path: __dirname + '/static'
-      , cache : { content: false }
-      })
-http.createServer(function(req, res){
-	console.log('Got a request');
-    mount(req, res);
-}).listen(1337)
+app.get('/jpeg-test', function (req, res) {
+  const nrImages =  req.query.nr || 10;
+  res.send(`<html><head></head><body>
+    ${mkImages(nrImages)}
+    </body></html>`);
+})
+ 
+app.listen(1337)
+// http://localhost:1337/ - zlib test
+// http://localhost:1337/jpeg-test?nr=1500 - sandbox scaling test
